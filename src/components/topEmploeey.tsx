c3e1f74f5a12cbeff38cb731ddd42b1ac74a101e
YehuditBrenig <138116736+YehuditBrenig@users.noreply.github.com>
2023-07-25 19:02:34 +0300

    topemploeey

---
+++ src/components/topEmploeey.tsx
@@ -0,0 +1,35 @@
+import { generateKey } from "crypto";
+import React from "react";
+import { Chart } from "react-google-charts";
+import blue from '@mui/material/colors/blue';
+import { redirect } from "react-router-dom";
+import styled from "@emotion/styled";
+
+// 
+export const data = [
+  ["emploeey", "orders"],
+  ["Michal", 11],
+  ["Oren", 2],
+  ["Helena", 2],
+  ["Dave", 2],
+  ["Eli", 7],
+];
+
+
+
+export const options = {
+backgroundColor:"lightGray"
+};
+
+export default function TopEmploeey() {
+  return (
+    <Chart  
+      chartType="PieChart"
+      data={data}
+     options={options}
+      width={"100%"}
+      height={"400px"}
+      
+    />
+  );
+}
