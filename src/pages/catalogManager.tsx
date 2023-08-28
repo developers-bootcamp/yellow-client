d0da766c2930f7b26410c30f0b85c88a2f76f408
AyalaSprei <ayala0bsd@gmail.com>
2023-08-28 16:26:02 +0300

    START

--- src/pages/catalogManager.tsx
+++ src/pages/catalogManager.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UseCrud } from "../redux/useCrud";
+import { IProductCatagory } from "../types/IProductCategories";
 
 const CatalogManager: React.FC = () => {
-
+    const { getData, postData, putData } = UseCrud();
+    const [categories, setCategories] = useState<IProductCatagory[]>([]);
+    const [products, setProducts] = useState([]);
+    const [newCategory, setNewCategory] = useState({ name: "", desc: "" });
+    const [updatedategory, setUpdatedCategory] = useState<IProductCatagory>();
+    const [showForm, setShowForm] = useState(false);
+    const [isNew, setIsNew] = useState(true);
+    
+    const getFunc = async (url: string) => {
+        let result = await getData(url);
+        if (url === "categories") setCategories(result);
+        if (url === "products") setProducts(result);
+      };
+    
+      useEffect(() => {
+        getFunc("categories");
+        getFunc("products");
+      }, []);
     return (
 
         <p>catalog Manager here</p>
