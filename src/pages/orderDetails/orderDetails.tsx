358e96955dcc2010ab53a893a0b340defebf2e77
miryam49469 <136648064+miryam49469@users.noreply.github.com>
2023-08-29 23:16:04 +0300

    a

--- src/pages/orderDetails/orderDetails.tsx
+++ src/pages/orderDetails/orderDetails.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Dialog from "@mui/material/Dialog";
 import { Link, DialogTitle } from "@mui/material";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
 import {
   DetailsDiv,
   GiftImg,
@@ -41,29 +44,57 @@ import { log } from "console";
 import { PALLETE } from "../../config/config";
 import { number } from "yup";
 import { left } from "@popperjs/core";
-
+import axios from "axios";
 export default function OrderDetails({ onClose, id }: any) {
   const { getData, postData, putData, deleteData } = UseCrud();
   const [products, setProducts] = useState<IProduct[]>([]);
   const [costumers, setCustomers] = useState<object[]>([]);
   const [orderItems, setOrderItems] = useState<IOrderItems[]>([]);
+  const [productResult, setProductResult] = useState<object[]>([]);
   const [sumOfPrice, setSumOfPrice] = useState<any>(0);
   const [selectedValueProduct, setSelectedValueProduct] = useState<
     string | null
   >(null);
   const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [order, setOrder] = useState<IOrder>();
   const [isOpen, setIsOpen] = useState(false);
   const [creditCardNumber, setCreditCardNumber] = useState();
   const [cvc, setCvc] = useState("");
-  const [expiryOn, setExpiryOn] = useState<Date>();
+  const [expiryOn, setExpiryOn] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [amount, setamount] = useState(0);
+  const [currencyMap, setCurrencyMap] = useState<CurrencyMap>({ key: 'ש"ח', value: 1 });
+  const [currency, setCurrency] = useState<string[]>([]);
+  const [selectedMenuItem, setSelectedMenuItem] = React.useState<string | null>(null);
+ interface CurrencyMap {
+    key: string;
+    value: number;
+  }
+  let flag = false
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   let arrey: IProduct[] = [];
   let arr = [];
+  const postFunc = async (url: string, body: object) => {
+    let result = await postData(url, body);
+    setProductResult(result);
+    if (url === "order/CalculateOrderAmount") {
+      alert(result)
+      // let val = result[-1];
+      // const targetValue = -1;
+      // const targetKey = Object.keys(val).find((key) => val[key] === targetValue);
+      // setSumOfPrice(targetKey);
+    }
+  }
   const getFunc = async (url: string) => {
     let result = await getData(url);
     if (url != "product") {
@@ -77,6 +108,9 @@ export default function OrderDetails({ onClose, id }: any) {
     }
     if (url == "product") {
       setProducts(result);
+    }
+    if (url == "GetCurrency") {
+      setCurrency(result);
     }
   };
   const handleInputChange = (
@@ -112,12 +146,9 @@ export default function OrderDetails({ onClose, id }: any) {
       orderItems: orderItems,
     }));
   };
-
-  const getCurrencyProducts = () => {
-    order?.orderItems.forEach((item) => {
-      arrey.push(item.productId);
-    });
-    setProducts(arrey);
+  const handleMenuItemClick = (text: string) => () => {
+    setSelectedMenuItem(text);
+    setAnchorEl(null);
   };
   const dellProduct = (name: string, amount: number) => {
     // let updateItems:IOrderItems
@@ -130,44 +161,42 @@ export default function OrderDetails({ onClose, id }: any) {
     }
     console.log("order after change", order);
     setOrderItems(updateItems);
-    reducePrice(prod?.productId?.price);
     setOrder((prevOrder: any) => ({
       ...prevOrder,
       totalAmount: sumOfPrice,
       orderItems: orderItems,
     }));
   };
-  const reducePrice = (price: number | undefined) => {
-    let summ = order?.totalAmount;
-    if (summ != undefined && price != undefined) {
-      let totalSum = summ - price;
-      setSumOfPrice(totalSum);
+  useEffect(() => {
+    if (selectedMenuItem == "DOLLAR") {
+      const x: CurrencyMap = { key: "$", value: 3.5 };
+      setCurrencyMap(x);
     }
-  };
-  //לסדר
-  const productList = [
-    { name: "Collage", price: "$10", quantity: "x 2" },
-    { name: "Photo albom", price: "$20", quantity: "x 3" },
-    // ... more items
-  ];
-  const user = [
-    { fullName: "Rachel Livne" },
-    { fullName: "Miryam Rozenbaom" },
-    // ... more items
-  ];
+  }, [selectedMenuItem])
   useEffect(() => {
     getFunc(`order/${id}`);
   }, []);
+  // useEffect(() => {
+  //   console.log("orderItemsBefore", orderItems)
+  //   postFunc("order/CalculateOrderAmount", { orderItems });
+  // }, [orderItems])
   useEffect(() => {
     if (products.length == 0) {
       getFunc("product");
     }
   }, []);
-
   useEffect(() => {
     console.log("miryam thank you", orderItems);
   }, [orderItems]);
-
+  const sameProd = (i: IOrderItems, quantuty: number) => {
+    orderItems.forEach(item => { if (item == i) { item.quantity = item.quantity + quantuty } })
+    setOrderItems(orderItems)
+    setOrder((prevOrder: any | undefined) => ({
+      ...prevOrder,
+      orderItems: orderItems,
+    }))
+    flag = true
+  }
   const addToCart = () => {
     let product: any;
     for (let i = 0; i < products.length; i++) {
@@ -176,41 +205,35 @@ export default function OrderDetails({ onClose, id }: any) {
         break;
       }
     }
-    let amount = quantity * product.price;
-    product = { productId: product, quantity: quantity, amount: amount };
-    setOrderItems((prevCart) => [...prevCart, product]);
-    // setPrice(product.amount)
-    setSumOfPrice(sumOfPrice + product.amount);
-    setOrder((prevOrder: any) => ({
-      ...prevOrder,
-      orderItems: orderItems,
-      totalAmount: sumOfPrice,
-    }));
-  };
-
-  const setPrice = (price: number) => {
-    let sum = order?.totalAmount;
-    if (sum != undefined) {
-      let totalSum = sum + price;
-      setSumOfPrice(totalSum);
+    // let amount = quantity * product.price;
+    product = { productId: product, quantity: quantity };
+    orderItems.forEach(item => {
+      if (item.productId.name == selectedValueProduct) {
+        sameProd(item, product.quantity);
+      }
+    })
+    if (!flag) {
+      setOrderItems((prevCart) => [...prevCart, product]);
+      // setPrice(product.amount)
+      // setSumOfPrice(sumOfPrice + product.amount);
+      setOrder((prevOrder: any) => ({
+        ...prevOrder,
+        orderItems: orderItems,
+        totalAmount: sumOfPrice,
+      }));
     }
   };
   const saveChanges = async (e: any) => {
     if (order) {
       console.log(order);
+       order.orderStatusId="approved"
+       setOrder(order)
       let result = await putData("order", order);
+      // let result = await axios.put("http://localhost:8080/order",order)
       console.log(result);
     }
   };
-  const cancelOrder = (id: string | undefined) => {};
-  const changeDetail = (e: any, type: string) => {
-    alert("הפרטים שונו בהצלחה");
-    console.log();
-  };
-  // useEffect(() => {
-  //     getCurrencyProducts()
-  // }, []);
-
+  const cancelOrder = (id: string | undefined) => { };
   return (
     <div >
       <DialogContent sx={{ p: 0, height: "30vh" }}>
@@ -231,18 +254,16 @@ export default function OrderDetails({ onClose, id }: any) {
                 <>
                   {getAllOrders()}
                   <div
-                    style={{ position: "absolute", left: "1vw", top: "10vh",width:"30vw" }}
+                    style={{ position: "absolute", left: "1vw", top: "10vh", width: "30vw" }}
                   >
                     <br></br>
-                  
-                      <FormHelperText>customer</FormHelperText>
-                      {/* <br></br> */}
-                      <TextField
-                        sx={{ width: "70%" }}
-                        value={order?.customer.fullName}
-                        aria-readonly
-                      ></TextField>
-                 
+                    <FormHelperText>customer</FormHelperText>
+                    {/* <br></br> */}
+                    <TextField
+                      sx={{ width: "70%" }}
+                      value={order?.customer.fullName}
+                      aria-readonly
+                    ></TextField>
                     <FormHelperText>Product</FormHelperText>
                     {/* <br></br> */}
                     <Autocomplete
@@ -258,33 +279,61 @@ export default function OrderDetails({ onClose, id }: any) {
                         <TextField {...params} label="Products" />
                       )}
                     />
-
                     <br></br>
-                    <div style={{position:"absolute",left:"51%",top:"16.5vh"}}>
-                    <TextField
-                      type="number"
-                      label="quantity"
-                      sx={{ width: "40%" }}
-                      inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        handleInputChange("quantity", e);
-                      }}
-                    /></div>
-
+                    <div style={{ position: "absolute", left: "51%", top: "16.5vh" }}>
+                      <TextField
+                        type="number"
+                        label="quantity"
+                        sx={{ width: "50%" }}
+                        inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                          handleInputChange("quantity", e);
+                        }}
+                      />
+                    </div>
+                    {currency ? <>
+                      <div>
+                        <Button
+                          id="fade-button"
+                          aria-controls={open ? 'fade-menu' : undefined}
+                          aria-haspopup="true"
+                          aria-expanded={open ? 'true' : undefined}
+                          onClick={handleClick}
+                        >
+                          Currency
+                        </Button>
+                        <Menu
+                          id="fade-menu"
+                          MenuListProps={{
+                            'aria-labelledby': 'fade-button',
+                          }}
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                          TransitionComponent={Fade}
+                        >
+                          {currency.map((i) => (
+                            <MenuItem key={i} onClick={handleMenuItemClick(i)}>
+                              {i}
+                            </MenuItem>
+                          ))}
+                        </Menu>
+                      </div>
+                    </> : <></>}
                     <br></br>
-
+                    <br></br>
                     <Button
                       onClick={() => addToCart()}
                       sx={{
                         mt: 2,
                         backgroundColor: `${PALLETE.BLUE} !important`,
                         width: "70%",
-                        position:"absolute",
-                        top:"24vh",
+                        position: "absolute",
+                        top: "24vh",
                         color: `${PALLETE.WHITE} !important`,
                       }}
                       type="submit"
-                      //   disabled={!isValid}
+                    //   disabled={!isValid}
                     >
                       ADD
                     </Button>
@@ -293,7 +342,6 @@ export default function OrderDetails({ onClose, id }: any) {
               ) : (
                 <></>
               )}
-
               {sumOfPrice ? (
                 <div style={{ left: "42%", position: "absolute" }}>
                   price: {sumOfPrice}
@@ -302,7 +350,6 @@ export default function OrderDetails({ onClose, id }: any) {
                 <></>
               )}
             </Grid>
-
             <div style={{ position: "absolute", left: "44%", top: "20vh" }}>
               <Grid container spacing={2}>
                 <h5>productList:</h5>
@@ -334,7 +381,6 @@ export default function OrderDetails({ onClose, id }: any) {
             </div>
             <div style={{ position: "absolute", top: "40vh" }}>
               <Divider sx={{ mt: 6 }} />
-
               <Typography>
                 Paid with a credit card ending in digits:{" "}
                 {order?.creditCardNumber.toString().substring(12)}
@@ -343,7 +389,7 @@ export default function OrderDetails({ onClose, id }: any) {
                 {isOpen ? "CHANGE" : " change credit card details"}
               </Button>
               {isOpen && (
-                <div style={{position:"absolute",width:"60vw"}}>
+                <div style={{ position: "absolute", width: "60vw" }}>
                   <TextField
                     type="number"
                     label="creditCardNumber"
@@ -354,7 +400,7 @@ export default function OrderDetails({ onClose, id }: any) {
                     }}
                   />
                   <TextField
-                    type="month"
+                    type="string"
                     // label="expiers on"
                     sx={{ width: 120, textAlign: "left" }}
                     inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
@@ -373,49 +419,57 @@ export default function OrderDetails({ onClose, id }: any) {
                   />
                 </div>
               )}
-
               <br></br>
-              <div style={{top:"25vh",position:"absolute",width:"100%"}}>
-                <div style={{position:"absolute"}}>
-                <Button
-                  onClick={() => cancelOrder(order?.id)}
-                  sx={{
-                    mt: 2,
-                    backgroundColor: `${PALLETE.ORANGE} !important`,
-                    width: "17vw",
-                    color: `${PALLETE.WHITE} !important`,
-                  }}
-                  type="submit"
+              <div style={{ top: "25vh", position: "absolute", width: "100%" }}>
+                <div style={{ position: "absolute" }}>
+                  <Button
+                    onClick={() => cancelOrder(order?.id)}
+                    sx={{
+                      mt: 2,
+                      backgroundColor: `${PALLETE.ORANGE} !important`,
+                      width: "17vw",
+                      color: `${PALLETE.WHITE} !important`,
+                    }}
+                    type="submit"
                   //   disabled={!isValid}
-                >
-                  Cancel Order
-                </Button>
+                  >
+                    Cancel Order
+                  </Button>
                 </div>
-<div style={{position:"absolute",left:"80%"}}>
-                <Button
-                  sx={{
-                    mt: 2,
-                    left: "10% !important",
-                    backgroundColor: `${PALLETE.GREEN} !important`,
-                    width: "17vw",
-                    color: `${PALLETE.WHITE} !important`,
-                  }}
-                  type="submit"
-                  // disabled={!isValid}
-                  onClick={(e) => saveChanges(e)}
-                >
-                  Save Changes
-                </Button>
+                <div style={{ position: "absolute", left: "80%" }}>
+                  <Button
+                    sx={{
+                      mt: 2,
+                      left: "10% !important",
+                      backgroundColor: `${PALLETE.GREEN} !important`,
+                      width: "17vw",
+                      color: `${PALLETE.WHITE} !important`,
+                    }}
+                    type="submit"
+                    // disabled={!isValid}
+                    onClick={(e) => saveChanges(e)}
+                  >
+                    Save Changes
+                  </Button>
                 </div>
               </div>
             </div>
           </DialogContent>
         </DetailsDiv>
-        <BackImg sx={{height:"53vh !important", marginBottom:"0vh !important"}}>
-          <GiftImg src="gifts.png" sx={{height:"100% !imporant", marginBottom:"0vh !important"}}></GiftImg>
-          <TextSide sx={{position:"absolute",top:"20vh",zIndex:"10",left:"3vw"}}>we almoste done</TextSide>
+        <BackImg sx={{ height: "53vh !important", marginBottom: "0vh !important" }}>
+          <GiftImg src="gifts.png" sx={{ height: "100% !imporant", marginBottom: "0vh !important" }}></GiftImg>
+          <TextSide sx={{ position: "absolute", top: "20vh", zIndex: "10", left: "3vw" }}>we almoste done</TextSide>
         </BackImg>
       </DialogContent>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
